90721e152d486197644aeb5973d6e6e0fc521a81
pratapsingh999 <108220183+pratapsingh999@users.noreply.github.com>
2024-09-19 17:37:51 +0530

    veriables,datatypes

--- practice_variable.js
+++ practice_variable.js
@@ -57,4 +57,118 @@ const lastname = "thakur"
 
 
 
+// hoisting
+
+console.log(home, "home");
+var home = "good"          
+
+
+// var x;  // Declare x
+// x = 5;  // Assign 
+// var x = 5;  // Initialize
+
+
+// Hoisting
+// moving all declarations to the top of the current scope (declaration hoised to the top. but initialization is not)
+//  access before declaration is call hoisting (variable can be used before it has been declared.)
+
+x = 5; //assign
+console.log(x, "aasvvava");
+var x   //declaration
+
+
+// num = 6;  
+// console.log(num);  //Error if let and const used
+let num = 6;       // with var undefined is printed
+
+myhoisting()
+function myhoisting () {
+   console.log('hoisting >>>>>>>>>>>>>>')
+}
+
+// host()
+const host = () => {                  // ReferenceError: Cannot access 'host' before initialization
+   console.log('asdfghjkl')
+}
+
+
+//  primitive/value types or Data types
+//    (1. String 2.Number 3. Boolean 4. undefined 5. null)
+
+let names = 'pratapsingh';  // string
+// typeof(names)
+console.log(typeof names)
+
+let age = 25;              // number
+// typeof age 
+console.log(typeof age)
+
+let isapproved = true;     // boplean
+// typeof isapproved
+console.log(typeof isapproved)
+
+// let firstName = dggdg ;  // type undefined and value also undefined
+let firstNames = undefined ; 
+// typeof firstName
+console.log(typeof firstNames)
+
+let selectColor = null;     // null 
+// typeof selectColor
+console.log(typeof selectColor)
+
+
+// non primitive data type / reference types
+// (1. object , 2. Arrat , 3. Function)
+
+// 1. object
+let person = {
+   name : "pratap",
+   age: 25
+}
+// Dot Notation 
+person.name = 'singh'
+
+// Bracket Notation
+person['name'] = 'thakur'
+console.log(person.name)
+
+// 2. Array 
+
+ let colors = ['red', 'bule'];
+ colors[2] = "orange"
+ colors[3] = 1
+  console.log(colors)
+
+ let nums = [23,45,67,89,'polpo']
+ console.log(nums);
+
+//  3. function
+
+function greet () {
+   console.log('Goodmornig')
+}
+greet()
+
+function greet (Nmaes , lastname) {
+   console.log('Goodmornig' + Nmaes + lastname)
+}
+greet('pratap' , 'singh')
+greet('singh')
+
+function squae (number){
+   return number*5
+}
+console.log(squae(6));
+
+// let checkNull = {
+//    name:"pratap"
+// }
+// checkNull = "";
+// checkNull = null;
+
+// if(checkNull != "" || checkNull != null){
+//    checkNull = "mahi"
+// }
+
+
 
